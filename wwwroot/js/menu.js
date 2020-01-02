da87252f09e12a698fe16f494a82d29557e722cf
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2020-01-02 15:15:31 +0530

    Add files via upload

--- wwwroot/js/menu.js
+++ wwwroot/js/menu.js
@@ -4,6 +4,14 @@ $("#menu-toggle").click(function (e) {
     $("#wrapper").toggleClass("toggled");
 });
 
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
 
 function startDictation() {
 
